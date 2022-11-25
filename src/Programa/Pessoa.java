5c5cc8726530caca7946e51b30d8844baadccd30
Paulojoserc <pjrc83@gmail.com>
2022-11-24 22:52:16 -0300

    Iniciando o projeto

---
+++ src/Programa/Pessoa.java
@@ -0,0 +1,46 @@
+package Programa;
+
+public class Pessoa {
+	private static int counter =1;
+	private String nome;
+	private String CPF;
+	private String email;
+	
+	public Pessoa(String nome, String CPF, String email) {
+		super();
+		this.nome = nome;
+		this.CPF = CPF;
+		this.email = email;
+		counter+=1; 
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+ public String toString() {
+	 return "\nNome: "+ this.getNome() +
+			 "\nCPF: "+this.getCPF() +
+			 "\nEmail "+this.email;
+ }
+}
